1309109b13cc9ce48f3b859bffb30d2f30685a33
Nick Rout <nick@bitx.co>
2017-04-03 18:06:15 +0200

    Add new icons

--- app/src/main/java/com/nickrout/shortcuts/ui/ChoiceActivity.java
+++ app/src/main/java/com/nickrout/shortcuts/ui/ChoiceActivity.java
@@ -86,7 +86,7 @@ public class ChoiceActivity extends NoDisplayActivity {
         builder.setStyle(new NotificationCompat.BigTextStyle().bigText(mChoice.scenario))
                 .setContentTitle(getString(R.string.title_scenario))
                 .setPriority(mNotificationPriority)
-                .setSmallIcon(R.mipmap.ic_launcher) // TODO: Add dedicated small notification icon
+                .setSmallIcon(R.drawable.ic_shortcuts_black_24dp)
                 .setLargeIcon(BitmapUtil.drawableToBitmap(mChoice.getScenarioType().getIcon(this)))
                 .setColor(mChoice.getScenarioType().getColor(this))
                 .setLights(mChoice.getScenarioType().getColor(this), TIME_NOTIFICATION_LIGHTS, TIME_NOTIFICATION_LIGHTS)
